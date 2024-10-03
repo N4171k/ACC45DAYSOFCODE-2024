53281145f416229baaa1ba976e30a73aa6b41327
Naitik Tiwari <144876588+N4171k@users.noreply.github.com>
2024-10-03 09:49:24 +0530

    Create Day 6 - Easy - A or B.c

---
+++ Day 6 - Easy - A or B.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int n;
+    scanf("%d", &n);
+    int a,b,x,y;
+    while(n){
+        
+        scanf("%d %d", &a, &b);
+        
+        x=((500-(a*2))+(1000-((a+b)*4)));
+        y=((1000-(b*4))+(500-((a+b)*2)));
+        
+        if(y>x){
+            printf("%d\n", y);
+        }else{
+            printf("%d\n", x);
+        }
+        n--;
+    }
+}
+
