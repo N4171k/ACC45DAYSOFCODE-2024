ef6a8f63fb408fb6fb7a2cf9e633892516ad1021
Naitik Tiwari <144876588+N4171k@users.noreply.github.com>
2024-10-13 23:11:44 +0530

    Create Day 16 - Easy - Count the Notebooks.c

---
+++ Day 16 - Easy - Count the Notebooks.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int t,a;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &a);
+        printf("%d\n", a*10);
+    }
+}
+
